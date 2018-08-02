dc62229772b3859a6105c0d12f6002ba6f73a4e1
jaspreet <jaspreet@meddo.in>
2018-08-02 12:48:34 +0530

    test commit2

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
