127bb1a9204a8a9857212f7e4c58ce54f21094a1
jaspreet <jaspreet@meddo.in>
2018-08-02 11:37:24 +0530

    test commit

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.get('/', function(req, res) {
 });
  
  
-app.listen(8080, ip);
+app.listen(8095, ip, function() {
+	console.log("Listening on ");
+});
  
 module.exports = app;
+
+
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+// app.listen(server_port, server_ip_address, function () {
+//   console.log( "Listening on " + server_ip_address + ", port " + server_port )
+// });
